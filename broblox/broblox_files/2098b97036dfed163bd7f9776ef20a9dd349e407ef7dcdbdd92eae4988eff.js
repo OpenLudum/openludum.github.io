ea6bbfddc439acb9df1eb76b2f0810169cdf115e
OpenLudum <93741056+OpenLudum@users.noreply.github.com>
2021-11-18 17:12:04 +0000

    Add files via upload

---
+++ broblox/broblox_files/2098b97036dfed163bd7f9776ef20a9dd349e407ef7dcdbdd92eae4988eff.js
@@ -0,0 +1,4 @@
+var Roblox = Roblox || {};
+Roblox.LangDynamicDefault = Roblox.LangDynamicDefault || {};
+Roblox.LangDynamicDefault["Feature.PlacesList"] = {"Action.SeeAll":"See All","Label.CreatorBy":"By {creatorLink}","Label.PlayingPhrase":"{playerCount} Active","Label.PlacesListName":"Experiences","Label.ContextMenuTitle":"Experience","Action.JoinGame":"Join","Action.ViewDetails":"View Details","Label.GenericError":"An Error Occured","Label.NoGames":"No experiences found","Label.GenreExplorerName":"Categories"};
+window.Roblox && window.Roblox.BundleDetector && window.Roblox.BundleDetector.bundleDetected("DynamicLocalizationResourceScript_Feature.PlacesList");
